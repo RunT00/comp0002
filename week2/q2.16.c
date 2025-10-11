38e17131787d7c624beccbdb72828502ba18b162
RunT00 <thomaslin3206@gmail.com>
2025-10-11 19:06:42 +0100

    solved q2.16

--- week2/q2.16.c
+++ week2/q2.16.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-const int target = 100;
-
-const char digits[] = "123456789";
-
-void search(int pos, int current_sum, int last_operand, char expr[])
+void search(int pos, int current_sum, int last_operand, char expr[], int target, char digits[])
 {   
     if(pos == strlen(digits))
     {
@@ -32,20 +28,60 @@ void search(int pos, int current_sum, int last_operand, char expr[])
     {
         new_operand = last_operand*10 - next_digit;
     }
-    search(pos+1, current_sum-last_operand+new_operand, new_operand, new_expr);
+    search(pos+1, current_sum-last_operand+new_operand, new_operand, new_expr, target, digits);
 
     // 2 = '+'
     sprintf(new_expr, "%s+%d", expr, next_digit);
-    search(pos+1, current_sum+next_digit, next_digit, new_expr);
+    search(pos+1, current_sum+next_digit, next_digit, new_expr, target, digits);
 
     // 3 = '-'
     sprintf(new_expr, "%s-%d", expr, next_digit);
-    search(pos+1, current_sum-next_digit, -next_digit, new_expr);
+    search(pos+1, current_sum-next_digit, -next_digit, new_expr, target, digits);
 
+    // 4 = '*'
+    sprintf(new_expr, "%s*%d", expr, next_digit);
+    search(pos+1, current_sum-last_operand+last_operand*next_digit, last_operand*next_digit, new_expr, target, digits);
+
+    // 5 = '/'
+    if(last_operand%next_digit==0)
+    {
+        sprintf(new_expr, "%s/%d", expr, next_digit);
+        search(pos+1, current_sum-last_operand+last_operand/next_digit, last_operand/next_digit, new_expr, target, digits);
+    }
+    
+}
+
+void swap(char *a, char *b)
+{
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void permute(char digits[], int l, int r, int target)
+{
+    if(l == r)
+    {
+        char start_expr[2] = {digits[0], '\0'};
+        search(1, digits[0]-'0', 1, start_expr, target, digits);
+        return;
+    }
+
+    for(int i=l; i<=r; i++)
+    {
+        swap(&digits[l], &digits[i]);
+        permute(digits, l+1, r, target);
+        swap(&digits[l], &digits[i]);
+    }
 
 }
 
-int main()
+void main()
 {   
-    search(1, 1, 1, "1");
+    printf("Input the target number: ");
+    int target;
+    scanf("%d", &target);
+    char digits[] = "123456789";
+    permute(digits, 0, strlen(digits)-1, target);
+
 }
